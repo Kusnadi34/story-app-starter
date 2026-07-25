13b49a22739bafee29df190c3c9e2530b6c19bcc
Kusnadi34 <nanakusnadi035@gmail.com>
2026-07-25 22:54:48 +0700

    Refactor authentication and locale handling in app-header

--- src/js/components/app-header.js
+++ src/js/components/app-header.js
@@ -9,76 +9,100 @@ class AppHeader extends LitElement {
   constructor() {
     super();
     updateWhenLocaleChanges(this);
-    this.isAuth = false;
-    this._onAuthChange = this._onAuthChange.bind(this);
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    window.addEventListener('auth-changed', this._onAuthChange);
-    this._checkAuth();
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    window.removeEventListener('auth-changed', this._onAuthChange);
-  }
-
-  _checkAuth() {
-    this.isAuth = !!localStorage.getItem('token');
-    this.requestUpdate();
-  }
-
-  _onAuthChange() {
-    this._checkAuth();
   }
 
   render() {
     const currentLocale = window.__litLocalize?.getLocale() || 'en';
+    const isLoggedIn = !!localStorage.getItem('accessToken');
+    const userName = localStorage.getItem('userName') || '';
 
     return html`
       <nav class="navbar navbar-expand-lg custom-navbar">
         <div class="container">
           <a class="navbar-brand" href="#home">📖 ${msg('appTitle')}</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
+          <div
+            class="offcanvas offcanvas-end"
+            tabindex="-1"
+            id="offcanvasNavbar"
+          >
             <div class="offcanvas-header">
               <h5 class="offcanvas-title">${msg('appTitle')}</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="offcanvas"
+              ></button>
             </div>
             <div class="offcanvas-body">
               <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                ${this.isAuth ? html`
-                  <li class="nav-item">
-                    <a class="nav-link" href="#home">${msg('navHome')}</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#add">${msg('navAdd')}</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#profile">${msg('navProfile')}</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" @click=${this._handleLogout}>${msg('logout')}</a>
-                  </li>
-                ` : html`
-                  <li class="nav-item">
-                    <a class="nav-link" href="#login">${msg('login')}</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#register">${msg('register')}</a>
-                  </li>
-                `}
-                
+                ${isLoggedIn
+                  ? html`
+                      <li class="nav-item">
+                        <a class="nav-link" href="#home">${msg('navHome')}</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#add">${msg('navAdd')}</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#profile"
+                          >${msg('navProfile')}</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <span class="nav-link text-success">👤 ${userName}</span>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#" @click=${this._handleLogout}
+                          >${msg('logout')}</a
+                        >
+                      </li>
+                    `
+                  : ''}
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="languageDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
                     ${currentLocale === 'en' ? '🌐 EN' : '🌐 ID'}
                   </a>
-                  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="languageDropdown">
-                    <li><a class="dropdown-item ${currentLocale === 'en' ? 'active' : ''}" href="#" @click=${this._switchLocale} data-locale="en">English</a></li>
-                    <li><a class="dropdown-item ${currentLocale === 'id' ? 'active' : ''}" href="#" @click=${this._switchLocale} data-locale="id">Indonesia</a></li>
+                  <ul
+                    class="dropdown-menu dropdown-menu-end"
+                    aria-labelledby="languageDropdown"
+                  >
+                    <li>
+                      <a
+                        class="dropdown-item ${currentLocale === 'en'
+                          ? 'active'
+                          : ''}"
+                        href="#"
+                        @click=${this._switchLocale}
+                        data-locale="en"
+                        >English</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        class="dropdown-item ${currentLocale === 'id'
+                          ? 'active'
+                          : ''}"
+                        href="#"
+                        @click=${this._switchLocale}
+                        data-locale="id"
+                        >Indonesia</a
+                      >
+                    </li>
                   </ul>
                 </li>
               </ul>
@@ -89,27 +113,25 @@ class AppHeader extends LitElement {
     `;
   }
 
-  _handleLogout(e) {
+  _switchLocale(e) {
     e.preventDefault();
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    window.dispatchEvent(new CustomEvent('auth-changed'));
-    window.location.hash = '#login';
-    
+    const locale = e.currentTarget.dataset.locale;
+    window.dispatchEvent(
+      new CustomEvent('locale-changed', { detail: { locale } })
+    );
     const offcanvas = document.querySelector('#offcanvasNavbar');
     if (offcanvas) {
-      const bsOffcanvas = window.bootstrap.Offcanvas.getInstance(offcanvas);
+      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvas);
       if (bsOffcanvas) bsOffcanvas.hide();
     }
   }
 
-  _switchLocale(e) {
+  _handleLogout(e) {
     e.preventDefault();
-    const locale = e.currentTarget.dataset.locale;
-    window.dispatchEvent(new CustomEvent('locale-changed', { detail: { locale } }));
+    if (window.__logout) window.__logout();
     const offcanvas = document.querySelector('#offcanvasNavbar');
     if (offcanvas) {
-      const bsOffcanvas = window.bootstrap.Offcanvas.getInstance(offcanvas);
+      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvas);
       if (bsOffcanvas) bsOffcanvas.hide();
     }
   }
