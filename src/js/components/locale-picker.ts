b0c58ddff2ce662294f391f544a333b73668d62e
Kusnadi34 <nanakusnadi035@gmail.com>
2026-08-01 20:59:17 +0700

    Add LocalePicker component for language selection

---
+++ src/js/components/locale-picker.ts
@@ -0,0 +1,40 @@
+import { html } from 'lit';
+import { allLocales } from '../../generated/locale-codes';
+import { updateWhenLocaleChanges } from '@lit/localize';
+import { getLocale, localeNames, setLocaleFromUrl } from '../localization';
+import LitWithoutShadowDom from './base/lit-without-shadow-dom';
+
+class LocalePicker extends LitWithoutShadowDom {
+  constructor() {
+    super();
+    updateWhenLocaleChanges(this);
+  }
+
+  render() {
+    return html`
+      <select class="form-select w-auto m-auto" @change=${this._localeChanged}>
+        ${allLocales.map((locale) => {
+          return html`
+            <option value=${locale} ?selected=${locale === getLocale()}>
+              ${localeNames[locale] || locale}
+            </option>
+          `;
+        })}
+      </select>
+    `;
+  }
+
+  private _localeChanged(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    const newLocale = select.value;
+
+    if (newLocale !== getLocale()) {
+      const url = new URL(window.location.href);
+      url.searchParams.set('lang', newLocale);
+      window.history.pushState(null, '', url.toString());
+      setLocaleFromUrl();
+    }
+  }
+}
+
+customElements.define('locale-picker', LocalePicker);
