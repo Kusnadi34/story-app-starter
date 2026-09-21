c9f2661d14d22854a091c733749045c68da154f7
Kusnadi34 <nanakusnadi035@gmail.com>
2026-09-21 20:51:21 +0700

    Enhance production config with CSS extraction and minimization

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,38 @@
-const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
+  output: {
+    // ✅ Kunci perbaikannya ada di sini:
+    publicPath: '/story-app-starter/',
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, // Ekstrak CSS ke file terpisah
+          'css-loader',
+          'sass-loader',
+        ],
       },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
     ],
   },
 });
